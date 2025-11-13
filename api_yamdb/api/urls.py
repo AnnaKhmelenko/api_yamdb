9a4586871873c0938fbed1b49e1b632142d75a4a
Alex Boryslavsky <alexmonk@mail.ru>
2025-11-13 10:44:53 +0300

    "Удалил пустой файл с моделями, поправил юрл'

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
 from .views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 router = DefaultRouter()
@@ -9,5 +8,5 @@ router.register('genres', GenreViewSet, basename='genres')
 router.register('titles', TitleViewSet, basename='titles')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('v1/', include(router.urls)),
 ]
